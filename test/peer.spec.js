a2845da7d3c88b7f5e74336dcd323c4ad5ad68be
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2014-08-24 14:23:05 +0900

    Use spy objects

--- test/peer.spec.js
+++ test/peer.spec.js
@@ -1,69 +1,84 @@
-describe('Local', function () {
+describe('Peer', function () {
 
-    it('should listen to a local event', function () {
-        var local = new join.Local("local");
-        var count = 0;
-        var data;
-        local.on('local', function (d) {
-            count++;
-            data = d;
+    describe('Local', function () {
+        it('should listen to a local event', function () {
+            var spy = jasmine.createSpy('spy');
+
+            var local = new join.Local('local');
+            local.on('local', spy);
+            local.local();
+
+            expect(spy).toHaveBeenCalledWith(local);
         });
-        local.local();
-        expect(count).toBe(1);
-        expect(data).toBe(local);
+
+        it('should listen to a remote event', function () {
+            var spy = jasmine.createSpy('spy');
+
+            var local = new join.Local();
+            local.on('remote', spy);
+            var remote = new join.Remote(0, 'remote');
+            local.remote(remote);
+
+            expect(spy).toHaveBeenCalledWith(remote);
+        });
+
+        it('should listen to a stream event', function () {
+            var spy = jasmine.createSpy('spy');
+
+            var local = new join.Local();
+            var stream = {};
+            local.on('stream', spy);
+            local.stream(stream);
+
+            expect(spy).toHaveBeenCalledWith(stream);
+        });
+
+        it('should listen to a bundle event', function () {
+            var spy = jasmine.createSpy('spy');
+
+            var local = new join.Local();
+            var bundle = new join.ChannelBundle('chat', local, function() {});
+            local.on('chat.channel', spy);
+            local.bundle(bundle);
+
+            expect(spy).toHaveBeenCalledWith(bundle);
+        });
+
+        it('should listen to a close event', function () {
+            var spy = jasmine.createSpy('spy');
+
+            var local = new join.Local();
+            local.onclose = spy;
+            local.close();
+
+            expect(spy).toHaveBeenCalled();
+        });
+
     });
 
-    it('should listen to a remote event', function () {
-        var local = new join.Local();
-        var remote = new join.Remote(0, 'remote');
-        var count = 0;
-        var data;
-        local.on('remote', function (remote) {
-            count++;
-            data = remote;
-        });
-        local.remote(remote);
-        expect(count).toBe(1);
-        expect(data).toBe(remote);
-    });
+    describe('Remote', function () {
 
-    it('should listen to a stream event', function () {
-        var local = new join.Local();
-        var stream = {};
-        var count = 0;
-        var data;
-        local.on('stream', function (stream) {
-            count++;
-            data = stream;
-        });
-        local.stream(stream);
-        expect(count).toBe(1);
-        expect(data).toBe(stream);
-    });
+        it('should listen to a close event', function () {
+            var spy = jasmine.createSpy('spy');
 
-    it('should listen to a bundle event', function () {
-        var local = new join.Local();
-        var bundle = new join.ChannelBundle('chat', local, function() {});
-        var count = 0;
-        var data;
-        local.on('chat.channel', function (bundle) {
-            count++;
-            data = bundle;
-        });
-        local.bundle(bundle);
-        expect(count).toBe(1);
-        expect(data).toBe(bundle);
-    });
+            var remote = new join.Remote(0, 'remote');
+            remote.onclose = spy;
+            remote.close();
 
-    it('should listen to a close event', function () {
-        var local = new join.Local();
-        var count = 0;
-        var data;
-        local.on('close', function () {
-            count++;
+            expect(spy).toHaveBeenCalled();
         });
-        local.close();
-        expect(count).toBe(1);
+
+        it('should listen to a stream event', function () {
+            var spy = jasmine.createSpy('spy');
+
+            var remote = new join.Remote(0, 'remote');
+            var stream = {};
+            remote.on('stream', spy);
+            remote.stream(stream);
+
+            expect(spy).toHaveBeenCalledWith(stream);
+        });
+
     });
 
 });
