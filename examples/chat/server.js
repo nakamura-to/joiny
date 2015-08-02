8956705822da27daa56165f268b5ea83f576fa99
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2015-08-03 01:43:30 +0900

    Fix indentation

--- examples/chat/server.js
+++ examples/chat/server.js
@@ -34,39 +34,39 @@ websocketServer.on('request', function(request) {
   console.log('ws open: id=' + connection.id);
 
   connection.on('message', function(message) {
-      if (message.type === 'utf8') {
-          var signal = JSON.parse(message.utf8Data);
-          console.log('_on message: type =' + signal.type);
-          if (signal) {
-            if (signal.type === 'subscription') {
-              connection.key = signal.key;
-              connection.name = signal.src.name;
+    if (message.type === 'utf8') {
+      var signal = JSON.parse(message.utf8Data);
+      console.log('_on message: type =' + signal.type);
+      if (signal) {
+        if (signal.type === 'subscription') {
+          connection.key = signal.key;
+          connection.name = signal.src.name;
+          console.log('ws message:' +
+            ' key=' + connection.key +
+            ' id=' + connection.id + '->id=' + connection.id +
+            ' type=' + signal.type);
+          var peers = connections.filter(function(c) {
+            return c.id !== connection.id && c.key === connection.key;
+          }).map(function (c) {
+            return {id: c.id, name: c.name};
+          });
+          connection.send(JSON.stringify({type: 'subscription', id: connection.id, peers: peers}), logError);
+        } else {
+          var dest = signal.dest;
+          connections.forEach(function(c) {
+            if (c.id === dest.id && c.key == connection.key) {
               console.log('ws message:' +
                 ' key=' + connection.key +
-                ' id=' + connection.id + '->id=' + connection.id +
+                ' id=' + connection.id + '->id=' + c.id +
                 ' type=' + signal.type);
-              var peers = connections.filter(function(c) {
-                return c.id !== connection.id && c.key === connection.key;
-              }).map(function (c) {
-                return {id: c.id, name: c.name};
-              });
-              connection.send(JSON.stringify({type: 'subscription', id: connection.id, peers: peers}), logError);
-            } else {
-              var dest = signal.dest;
-              connections.forEach(function(c) {
-                if (c.id === dest.id && c.key == connection.key) {
-                  console.log('ws message:' +
-                    ' key=' + connection.key +
-                    ' id=' + connection.id + '->id=' + c.id +
-                    ' type=' + signal.type);
-                  c.send(message.utf8Data, logError);
-                }
-            });
+              c.send(message.utf8Data, logError);
             }
-          } else {
-            console.log('invalid signal: ' + message.utf8Data);
-          }
+          });
+        }
+      } else {
+        console.log('invalid signal: ' + message.utf8Data);
       }
+    }
   });
 
   connection.on('close', function() {
